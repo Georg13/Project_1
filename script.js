83713fa021e78f9aae18ff9a1242648fa97f5358
Georg <geokav87@mail.ru>
2021-11-04 21:01:34 +0300

    Project_1.2

--- script.js
+++ script.js
@@ -10,6 +10,54 @@ let personalMovieDB = {
     private: false
 };
 
+// Var 1
+
+for(let i =0; i < 2; i++) {
+   let a = prompt('Один из полседних просмотренных фильмов?', ''),
+   b = prompt('На сколько оцените его?', '');
+
+        if(a && b && a.length < 50){
+            personalMovieDB.movies[a] = b;
+        } else {
+            //alert("Error!");
+            console.log("Error!");
+           i--;
+        }
+}
+
+// Var 1.1
+/*
+for(let i =0; i < 2; i++) {
+    let a = prompt('Один из полседних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+ 
+    if(a != null && b != null && a != "" && b != "" && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log('Done!')
+    } else {
+        console.log("Error!");
+    i--;
+    }
+ }
+
+
+// Var 2
+/*
+let i = 0;
+
+while(i < 2) {   
+    let a = prompt('Один из полседних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
+ 
+         if(a && b && a.length < 50){
+             personalMovieDB.movies[a] = b;
+             i++;
+         } else {
+             alert("Error!");
+         }
+ }
+*/
+/*
 const a = prompt('Один из полседних просмотренных фильмов?', ''),
       b = prompt('На сколько оцените его?', ''),
       c = prompt('Один из полседних просмотренных фильмов?', ''),
@@ -17,5 +65,15 @@ const a = prompt('Один из полседних просмотренных ф
 
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
+*/
+if(personalMovieDB.count < 10 ) {
+    console.log('Просмотренно довольно мало фильмов');
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель'); 
+} else if(personalMovieDB.count >= 30){ 
+    console.log('Вы киноман!');
+} else {
+    console.log('Какаято ошибка!');
+}
 
 console.log(personalMovieDB);
